3d44a3a67e112fedb4204cf23d943b726c2e7d7d
hesham2611 <heshamsaleh100100@gmail.com>
2025-08-14 11:38:30 +0300

    ATO update

--- _site/assets/js/data/swconf.js
+++ _site/assets/js/data/swconf.js
@@ -1,6 +1,6 @@
 const swconf = {
   
-    cacheName: 'chirpy-1755159679',resources: [
+    cacheName: 'chirpy-1755160682',resources: [
       '/assets/css/jekyll-theme-chirpy.css',
       '/',
       
